507e0b53d13c6b27096131dd7f5fdbc392cee56e
Martin <10461942@ucn.dk>
2026-04-16 22:42:28 +0200

    da

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,55 @@
 
 // Alle opskrifter
-const baseUrl = "https://api.martinnguyen.dk/wp-json/";
-
-const postsUrl = "wp/v2/posts";
+const baseUrl = "https://api.martinnguyen.dk/wp-json/wp/v2/posts";
 
 getAllPosts()
+    
 //  henter alle public posts
 function getAllPosts() {
     fetch(baseUrl)
         .then(res => res.json())
-        .then(data => renderArticles1(data))
+        .then(data => renderArticles(data, ".container"))
         .catch(err => console.log("Fejl: ", err));
 }
 
-function renderArticles1(posts, selector) {
+function renderArticles(posts, selector) {
     // tilføjet en selector i loopet
-    const glutenEl = document.querySelector(selector);
+    const allRecipes = document.querySelector(selector);
     console.log('posts:', posts)
+
     posts.forEach(post => {
-        let ingredients = [];   
-        for (const key in post.acf.diaet) {
-            const value = post.acf.diaet[key];
+        // ingredients
+        let ingredients = [];
+        for (const key in post.acf.ingredienser) {
+            const value = post.acf.ingredienser[key];
+
             if (value) {
-                ingredients.push(value)
+                ingredients.push(value);
             }
         }
-        console.log('ingredients:', ingredients)
-        glutenEl.innerHTML += `
+        // directions
+        let directions = [];
+        for (const key in post.acf.fremgangsmade) {
+            const value = post.acf.fremgangsmade[key];
+
+            if (value) {
+                directions.push(value);
+            }
+        }
+
+        allRecipes.innerHTML += `
         <article>
-            <h2>${post.acf.titel}</h2>
-            <img src="${post.acf.primaer_billede.sizes.medium_large}"/>
-            <p>${post.acf.beskrivelse}</p>
-            <h2>ingredienser</h2>
-            <ul>
+        <h2>${post.acf.titel}</h2>
+        <img src="${post.acf.primaer_billede}"/>
+        <p>${post.acf.beskrivelse}</p>
+        <h2>Ingredients</h2>
+        <ul>
             ${ingredients.map(ing => `<li>${ing}</li>`).join("")}
-            </ul>
-            <p class="author">${post.acf.forfatter[0].post_title}</p>
+        </ul>
+        <h2>Directions</h2>
+        <ul>
+            ${directions.map(ing => `<li>${ing}</li>`).join("")}
+        </ul>
         </article>`;
     })
-}
-
-
-// Enkelte opskrifter
-
-const rec = getRecipes();
-
-function getRecipes() {
-    fetch(baseUrl)
-        .then(res => res.json())
-        .then(data => renderArticles1(data))
-        .catch(err => console.log("Fejl: ", err));
 }
