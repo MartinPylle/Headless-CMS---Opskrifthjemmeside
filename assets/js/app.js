af41a7f4dcd6baa2c712a88dc2bb5b1ee1248823
Martin <10461942@ucn.dk>
2026-04-16 10:43:48 +0200

    dadada

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,66 +4,13 @@ const baseUrl = "https://api.martinnguyen.dk/wp-json/";
 
 const postsUrl = "wp/v2/posts";
 
-function getAllPosts() {
-    fetch(baseUrl)
-    .then(res => res.json())
-    .then(data => renderArticles1(data))
-    .catch(err => console.log("fejl", err))
-}
-
-// getAllPosts()
- 
-// getAllPostsByCategory(8);
-
-getToken()
-    // Hent nyt id med diæt id 10 fra id 8 
-    .then(() => getAllPrivatePostsByCategory(10))
-
-    
-
-async function getToken() {
-
-    const userLogin = {
-        username: "MartinHNguyen",
-        password: "dCuc vC9H kS4I QHw0 Wlu9 iUIH"
-    }
-
-    fetch(baseUrl + "jwt-auth/v1/token", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(userLogin)
-    })
-    .then(res => res.json())
-    .then(data => {
-        sessionStorage.setItem("myToken", data.token)
-    })
-    .catch(err => console.log("Det gik galt i getToken!!", err))
-}
-
+getAllPosts()
 //  henter alle public posts
 function getAllPosts() {
     fetch(baseUrl)
         .then(res => res.json())
         .then(data => renderArticles1(data))
         .catch(err => console.log("Fejl: ", err));
-}
-
-// Henter alle private posts 
-async function getAllPrivatePostsByCategory(id) {
-    try {
-        // Ændret til ?acf_format=standard&status=private&diaet fra categori
-        const res = await fetch(baseUrl + postsUrl + "?acf_format=standard&status=private&diaet=" + id, {
-            headers: {
-                Authorization: "Bearer " + sessionStorage.getItem("myToken")
-            }
-        });
-        const posts = await res.json();
-        renderArticles1(posts, ".glutenfri")
-    } catch (err) {
-        console.log('error:', err)
-    }
 }
 
 function renderArticles1(posts, selector) {
