f420bff0cebc5a293d7a532932a28c904fb3d182
Martin <10461942@ucn.dk>
2026-04-20 21:42:48 +0200

    da

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,104 @@
-// // Alle opskrifter ændre per_page= til din antal opskrifter du vil hente
-// const baseUrl = "https://api.martinnguyen.dk/wp-json/wp/v2/posts?per_page=1&acf_format=standard";
+// Alle opskrifter ændre per_page= til din antal opskrifter du vil hente
+const baseUrl = "https://api.martinnguyen.dk/wp-json/wp/v2/posts?per_page=25&acf_format=standard";
 
-// getAllPosts()
+getCategories().then(() => getAllPosts());
+
+
+
+//  henter alle public posts
+function getAllPosts() {
+    fetch(baseUrl)
+        .then(res => res.json())
+        .then(data => renderArticles(data, ".recipe-cards"))
+        .catch(err => console.log("Fejl: ", err));
+}
+
+let categoriesMap = {};
+
+function getCategories() {
+    return fetch("https://api.martinnguyen.dk/wp-json/wp/v2/categories")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(cat => {
+            categoriesMap[cat.id] = cat.name;
+        });
+    });
+}
+
+    function getDifficultyLevel(value) {
+        if (!value) return 1;
+
+        const v = value.toLowerCase().trim();
+
+        if (v === "easy") return 1;
+        if (v === "medium") return 2;
+        if (v === "hard") return 3;
+        return 1;
+    }
+
+function renderDifficulty(level) {
+    let html = "";
+
+    for (let i = 1; i <= level; i++) {
+        html += `<span class="kokkehuer">👨‍🍳</span>`;
+    }
+
+    return html;
+}
+
+function renderArticles(posts, selector) {
+    const allRecipes = document.querySelector(selector);
+    console.log('posts:', posts)
+
     
-// //  henter alle public posts
-// function getAllPosts() {
-//     fetch(baseUrl)
-//         .then(res => res.json())
-//         .then(data => renderArticles(data, ".container"))
-//         .catch(err => console.log("Fejl: ", err));
-// }
+    posts.forEach(post => {
+        
+        let difficultyLevel = getDifficultyLevel(post.acf.svaerhedsgrad);
+        let difficultyHTML = renderDifficulty(difficultyLevel);
+        // slet
+        // let courseName = categoriesMap[post.acf.course] || "";
+        // console.log('courseName:', courseName)
+
+        let imageUrl = "";
+        if (post.acf.picture) {
+            imageUrl = post.acf.picture.url;
+        }
+        // slet 
+        // let courseUrl = "";
+        // if (post.acf.course) {
+        //     courseUrl = post.acf.course
+        // }
+
+        allRecipes.innerHTML += `
+        <article class="recipe-card">
+				<div class="image-recipe-wrapper">
+					<img class="recipe-picture" src="${imageUrl}" alt="">
+					<div class="course">Breakfast</div>
+				</div>
+				<div class="recipe-wrapper">
+					<h3>${post.acf.titel}</h3>
+					<div class="minutes-difficulty">
+						<div class="time">
+							<p>Total time</p>
+							<p>${post.acf.tid_i_alt}<i class="fa-regular fa-clock"></i></p>
+						</div>
+						<div class="difficulty">
+							<p>Difficulty</p>
+							<div class="kokkehuer"> 
+                            ${difficultyHTML} </div>
+						</div>
+					</div>
+				</div>
+				<button id="makeNow-button">Make now</button>
+			</article>`;
+    });
+}
+
+ // let difficultyUrl = "";
+        // if(post.acf.svaerhedsgrad) {
+        //     difficultyUrl = post.acf.svaerhedsgrad.url
+        // }
+
 
 // function renderArticles(posts, selector) {
 //     const allRecipes = document.querySelector(selector);
@@ -55,26 +144,19 @@
 
 
 
-// Opskrifter filtering course, time and preferences
 
-// //  ændre per_page= til din antal opskrifter du vil hente
-// const baseUrl = "https://api.martinnguyen.dk/wp-json/wp/v2/posts?categories=BREAKFAST_ID&acf_format=standard";
-//     console.log('baseUrl:', baseUrl)
+// Alle opskrifter ændre per_page= til din antal opskrifter du vil hente
+// const baseUrl = "https://api.martinnguyen.dk/wp-json/wp/v2/posts?per_page=1&acf_format=standard";
+
+// getAllPosts()
     
-// //  henter posts med categori
-// fetch(baseUrl)
-//     .then(res => res.json())
-//     .then(data => {
-//         const filtered = data.filter(post => {
-//             return post.acf.tid && post.acf.tid <= 30;
-//         });
-
-//         renderArticles(filtered, ".container");
-//     })
-//     .catch(err => console.log(err));
-
-
-
+// //  henter alle public posts
+// function getAllPosts() {
+//     fetch(baseUrl)
+//         .then(res => res.json())
+//         .then(data => renderArticles(data, ".container"))
+//         .catch(err => console.log("Fejl: ", err));
+// }
 
 // function renderArticles(posts, selector) {
 //     const allRecipes = document.querySelector(selector);
