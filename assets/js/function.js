d33f3d655de6e1e0fab07244004ae34679607f34
Martin <10461942@ucn.dk>
2026-04-21 11:01:35 +0200

    k

--- assets/js/function.js
+++ assets/js/function.js
@@ -12,28 +12,3 @@ document.querySelector(".time-button").addEventListener("click", () => {
 document.querySelector(".dietary-button").addEventListener("click", () => {
     document.querySelector(".dietary-dropdown").classList.toggle("active");
 });
-
-
-// // course
-// document.querySelectorAll(".course-dropdown p").forEach(item => {
-//     item.addEventListener("click", () => {
-//         filters.course = item.dataset.value;
-//         updateUI();
-//     });
-// });
-
-// // time
-// document.querySelectorAll(".time-dropdown p").forEach(item => {
-//     item.addEventListener("click", () => {
-//         filters.time = item.dataset.value;
-//         updateUI();
-//     });
-// });
-
-// // dietary
-// document.querySelectorAll(".dietary-dropdown p").forEach(item => {
-//     item.addEventListener("click", () => {
-//         filters.dietary = item.dataset.value;
-//         updateUI();
-//     });
-// });
